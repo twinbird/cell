a0ab6987fecf6d3c4971fdb21683422610b5ede4
twinbird <ixa2063@gmail.com>
2021-04-14 07:36:44 +0000

    refactor

--- main.go
+++ main.go
@@ -6,54 +6,58 @@ import (
 	"os"
 )
 
-var (
-	spreadsheet    *Spreadsheet
-	program        string
-	optionTopath   string
-	optionFrompath string
-)
+type ExecContext struct {
+	code        string
+	topath      string
+	frompath    string
+	spreadsheet *Spreadsheet
+	exitCode    int
+}
 
 func main() {
+	con := &ExecContext{}
+
 	f := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	f.StringVar(&optionTopath, "to", "", "output spreadsheet filepath")
-	f.StringVar(&optionFrompath, "from", "", "input spreadsheet filepath")
+	f.StringVar(&con.topath, "to", "", "output spreadsheet filepath")
+	f.StringVar(&con.frompath, "from", "", "input spreadsheet filepath")
 
 	f.Parse(os.Args[2:])
 
-	program = os.Args[1]
+	con.code = os.Args[1]
 
-	run()
+	run(con)
+	os.Exit(con.exitCode)
 }
 
-func beforeRun() {
-	sheet, err := NewSpreadsheet(optionFrompath, optionTopath)
+func beforeRun(con *ExecContext) {
+	sheet, err := NewSpreadsheet(con.frompath, con.topath)
 	if err != nil {
 		fatalError(err)
 	}
-	spreadsheet = sheet
+	con.spreadsheet = sheet
 }
 
-func afterRun() {
-	if optionTopath != "" {
-		if err := spreadsheet.writeSpreadsheet(); err != nil {
+func afterRun(con *ExecContext) {
+	if con.topath != "" {
+		if err := con.spreadsheet.writeSpreadsheet(); err != nil {
 			fatalError(err)
 		}
 	}
 }
 
-func run() {
-	beforeRun()
-	ret := execScript()
-	afterRun()
+func run(con *ExecContext) {
+	beforeRun(con)
+	ret := execScript(con)
+	afterRun(con)
 
-	os.Exit(ret)
+	con.exitCode = ret
 }
 
-func execScript() int {
+func execScript(con *ExecContext) int {
 	yyDebug = 1
 	yyErrorVerbose = true
 
-	lexer := NewLexer(program)
+	lexer := NewLexer(con.code)
 	yyParse(lexer)
 
 	return int(lexer.ast.eval())
